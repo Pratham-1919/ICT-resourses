f476bfeb5f601357580b7676fe10d484b4fe3f2f
Langaliya Pratham <prathamlangaliya17@gmail.com>
2024-11-08 19:21:29 +0530

    hgctfuhkhygy

---
+++ mian page 1.0/second page/script.js
@@ -0,0 +1,10 @@
+function sem3page() {
+    window.location.href = "/second%20page/sem-3_playlist/index.html";
+  }
+  function sem4page() {
+    window.location.href = "/second%20page/sem-4%20playlist/index.html";
+  }
+  function sem5page() {
+    window.location.href = "/second%20page/sem-5%20playlist/index.html";
+  }
+  
